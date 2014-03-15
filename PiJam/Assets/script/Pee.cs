205c9ff77ee92b903d4925bbe2b612f2802bf381
Wenguang Xu <Wenguang_Xu@gnwc.ca>
2014-03-15 10:47:36 -0700

    bullet script

---
+++ PiJam/Assets/script/Pee.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag=="Enemy")
+		{
+			other.gameObject.SendMessage("Die");
+			Destroy(this.gameObject);
+		}
+
+		if(other.gameObject.tag=="UpperPlatform")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
