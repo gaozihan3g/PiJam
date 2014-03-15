896fc8eaef4c6e82bc5f5172bfed431e8a9dbcdf
Zihan Gao <gao_zihan@126.com>
2014-03-15 10:07:56 -0700

    enemy 0.1

---
+++ PiJam/Assets/script/AI/PiEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PiEnemy : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody2D.velocity = new Vector2(speed * Mathf.Sign(transform.localScale.x), 0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			print("GameOver");
+		}
+	}
+}
