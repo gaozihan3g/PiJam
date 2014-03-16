86f602e0362f32a6f46c8d4875c73b825aeda9fb
Zihan Gao <gao_zihan@126.com>
2014-03-16 12:34:38 -0700

    drop bomb sfx

--- PiJam/Assets/script/AI/PiBird.cs
+++ PiJam/Assets/script/AI/PiBird.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class PiBird : MonoBehaviour {
 
 	public GameObject pooPrefab;
+	public AudioClip dropSFX;
 
 	void Poo()
 	{
 		print("Poo");
+		AudioManager.Instance.PlayOneShot(dropSFX);
 		Instantiate(pooPrefab, transform.position, Quaternion.identity);
 	}
 }
